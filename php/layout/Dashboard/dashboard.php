642db4a139153955e05c61a6ec248c3ea844a5e2
Wiktor Luck <114016021+wiktorluck@users.noreply.github.com>
2023-10-28 13:43:53 +0200

    Update: files structure

---
+++ php/layout/Dashboard/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+require_once("../../../includes/authorized.php");
+?>
+
+
+<!doctype html>
+<html lang="pl">
+  <head>
+    <title>Dashboard</title>
+  </head>
+  <body>
+    
+      <?php
+        echo "Witaj ".$_SESSION['login'].'!';
+      ?>
+  
+      <a href="dashboard.php"><button>Dashboard</button></a>
+      <a href="../Products/products.php"><button>Products</button></a>
+      <a href="../Users/users.php"><button>Users</button></a>
+
+      <a href="../../auth/logout.php"><button>Wyloguj się</button></a>
+  </body>
+
+</html>
