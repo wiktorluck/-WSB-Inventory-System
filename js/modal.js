44248b704ff38450b071b1569bb42f71c1d2c9a5
Wiktor Luck <114016021+wiktorluck@users.noreply.github.com>
2023-12-20 15:02:33 +0100

    Delete js/modal.js

--- js/modal.js
+++
@@ -1,8 +0,0 @@
-//Notification Modal Script
-document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "block";
-    setTimeout(function() {
-        modal.style.display = "none";
-    }, 3000);
-});
