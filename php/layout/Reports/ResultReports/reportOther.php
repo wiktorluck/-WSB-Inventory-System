49798e5bb1a7f70b0ef2e9c143382a867ebb9607
Wiktor Luck <114016021+wiktorluck@users.noreply.github.com>
2023-12-26 13:51:23 +0100

    Create: Generating reports module

---
+++ php/layout/Reports/ResultReports/reportOther.php
@@ -0,0 +1,92 @@
+<?php
+require_once("../../../../includes/authorized.php");
+require_once("../../../../includes/modal_info.php");
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <link rel="icon" type="image/x-icon" href="images/inventura_logo_small.png">
+  
+  <meta charset="utf-8">
+  <meta name="description" content="System Inwentaryzacji Sprzętu Komputerowego">
+  <meta name="author" content="">
+  <meta name="generator" content="">
+  <link rel="stylesheet" href="../../../../css/loginform.css">
+  <link rel="stylesheet" href="../../../../css/body_style.css">
+  <link rel="stylesheet" href="../../../../css/notification_modals.css">
+  <title>INVENTURA</title>
+</head>
+
+<body>
+<div class="summaryform">
+
+
+        <p>Raport Pozostałych Sprzętów:<br>
+            </br></br>
+            <hr>
+            <?php
+            require_once "../../../../includes/connect.php";
+
+            $conn = @new mysqli($host, $db_user, $db_password, $db_name);
+            if ($conn->connect_errno != 0) {
+                echo "Error: " . $conn->connect_errno;
+            } else {
+                $sql = "SELECT * FROM products WHERE categoryp NOT IN ('Komputer PC', 'Laptop');";
+                $result = $conn->query($sql);
+
+                if ($result && $result->num_rows > 0) {
+                    echo '<table border="1">';
+                    echo '
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nazwa</th>
+                            <th>Kategoria</th>
+                            <th>Nr seryjny</th>
+                            <th>Nr ewidencyjny</th>
+                            <th>Cena ewidencyjna</th>
+                        </tr>
+                    </thead>';
+                    echo '<tbody>';
+
+                    $deficit = 0;
+                    $shortcomings = 0;
+
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<tr>';
+                        echo '<td>' . $row['idp'] . '</td>';
+                        echo '<td>' . $row['namep'] . '</td>';
+                        echo '<td>' . $row['categoryp'] . '</td>';
+                        echo '<td>' . $row['serialp'] . '</td>';
+                        echo '<td>' . $row['registrationp'] . '</td>';
+                        echo '<td>' . $row['pricep'] . ' zł</td>';
+                        echo '</tr>';
+                    }
+
+                    echo '</tbody></table>';
+                } else {
+                    echo "Brak wyników spełniających kryteria.";
+                }
+            }
+            ?>
+            <br><br><br>
+            
+            <br>
+            <div class="loginbutton"><button onclick="printPage()">Wydruk</button></div>
+            <a href="../reports.php"><div class="loginbutton"><button type="submit" name="logout">Wstecz</button></div></a>
+    </form>
+            
+    <div class="footer">INVENTURA @ 2023</div>
+</div>
+</body>
+
+
+<script>
+        function printPage() {
+            window.print();
+        }
+    </script>
+</html>
+
--- php/layout/Reports/reports.php
+++ php/layout/Reports/reports.php
@@ -19,27 +19,59 @@
 <div class="mainbox">
   <div class="welcometext"> <?php echo "Witaj z powrotem, ".$_SESSION['login'].'!'; ?>  </div>
 
-  <div class="summaryboxes">
-    <div class="box1"> Raport Braków 
-            <p> click </p>
+<?php
+if($_SESSION['activeInventory'] == 1 ){
+        echo '
+        
+        <div class="summaryboxes">
+        <h2>Raporty Inwentaryzacji</h2></br>
+        <hr>
+                <a href="ResultReports/reportDeficiencies.php">
+                 <div class="box1"> Raport Braków 
+                  <p> click </p>
+                 </div>
+                </a>
+
+
+                <a href="ResultReports/reportUncheckedPositions.php">
+                 <div class="box2"> Raport Niesprawdzonych Pozycji
+                  <p> click </p>
+                 </div>
+                </a>
+
+                <a href="ResultReports/reportCheckedPositions.php">
+                 <div class="box3"> Raport Sprawdzonych Pozycji 
+                  <p> click </p>
+                 </div>
+                </a>
+        </div>
+        ';
+}
+
+?>
+<div class="summaryboxes">
+<h2>Raporty aktualnych stanów</h2></br>
+<hr>
+
+<a href="ResultReports/reportComputers.php">
+    <div class="box4"> <h3>Raport Komputerów</h3>
+     <p> click </p>
     </div>
-    <div class="box2"> Raport Niesprawdzonych Pozycji
-            <p> click </p>
-    </div>
-    <div class="box3"> Raport Sprawdzonych Pozycji 
-            <p> click </p>
-    </div>
-</div>
-<div class="summaryboxes2">
-    <div class="box4"> Raport Komputerów
-            <p> click </p>
-    </div>
+</a>
+
+<a href="ResultReports/reportLaptops.php">
     <div class="box5"> Raport Laptopów 
-            <p> click </p>
+     <p> click </p>
     </div>
+</a>
+
+<a href="ResultReports/reportOther.php">
     <div class="box6"> Raport Innych Sprzętów 
-            <p> click </p>
+     <p> click </p>
     </div>
+</a>
+
+
   </div>
 </div>
 </body>
